3a6767a302c5e396b414541854734d0e702a4563
vimbing <jakubbubak0@gmail.com>
2022-12-19 07:44:27 +0100

    addded renegotiation'

--- u_parrots.go
+++ u_parrots.go
@@ -668,6 +668,7 @@ func utlsIdToSpec(id ClientHelloID) (ClientHelloSpec, error) {
 				TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
 				TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
 				TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+				0x00,
 			},
 			CompressionMethods: []byte{
 				compressionNone,
