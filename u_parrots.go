d340cfb308f954b22bd0299599c4143a4d60999b
vimbing <jakubbubak0@gmail.com>
2022-12-19 08:44:39 +0100

    test

--- u_parrots.go
+++ u_parrots.go
@@ -668,11 +668,10 @@ func utlsIdToSpec(id ClientHelloID) (ClientHelloSpec, error) {
 				TLS_CHACHA20_POLY1305_SHA256,
 				TLS_AES_128_GCM_SHA256,
 				TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 				0x9F,
-				OLD_TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
-				OLD_TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+				0xCC,
 				0xA8,
-				0x2B,
 			},
 			CompressionMethods: []byte{
 				compressionNone,
